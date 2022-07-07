86f2f0fb5513e9b1529fbd830576d9832c580797
Emilio Schepis <emilio.schepis@gmail.com>
2022-07-07 21:48:28 +0200

    feat: display label translations

--- app/routes/dashboard/$project/index.tsx
+++ app/routes/dashboard/$project/index.tsx
@@ -4,41 +4,57 @@ import { v4 as uuid } from "uuid";
 
 import { db } from "~/lib/db.server";
 import { requireUserId } from "~/lib/session.server";
-import {
-  getProject,
-  getProjectWithLabelsAndKey,
-} from "~/models/project.server";
 import { notFound } from "~/utils/responses";
 
 type LoaderData = {
-  project: NonNullable<Awaited<ReturnType<typeof getProjectWithLabelsAndKey>>>;
+  project: NonNullable<Awaited<ReturnType<typeof getProject>>>;
 };
+
+async function getProject(name: string) {
+  return db.project.findUnique({
+    where: { name },
+    include: {
+      apiKey: true,
+      locales: true,
+      labels: {
+        include: {
+          translations: true,
+        },
+      },
+    },
+  });
+}
 
 export const action: ActionFunction = async ({ request, params }) => {
   const userId = await requireUserId(request);
+  const form = await request.formData();
+
+  const intent = form.get("intent");
 
   const project = await getProject(params.project as string);
   if (!project || project.userId !== userId) {
     throw notFound();
   }
 
-  await db.apiKey.upsert({
-    where: { projectId: project.id },
-    create: {
-      projectId: project.id,
-      key: uuid(),
-    },
-    update: {
-      key: uuid(),
-    },
-  });
+  if (intent === "regenerate-api-key") {
+    await db.apiKey.upsert({
+      where: { projectId: project.id },
+      create: {
+        projectId: project.id,
+        key: uuid(),
+      },
+      update: {
+        key: uuid(),
+      },
+    });
+  }
 
   return new Response("OK", { status: 200 });
 };
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   const userId = await requireUserId(request);
-  const project = await getProjectWithLabelsAndKey(params.project as string);
+  const project = await getProject(params.project as string);
 
   if (!project || project.userId !== userId) {
     throw notFound();
@@ -55,21 +71,43 @@ export default function ProjectRoute() {
   return (
     <div>
       <h1>{data.project.name}</h1>
-      <h2>Labels</h2>
       <details>
         <summary>Api Key</summary>
         {data.project.apiKey ? <code>{data.project.apiKey.key}</code> : null}
-        <Form method="post">
+        <Form method="post" replace>
+          <input type="hidden" name="intent" value="regenerate-api-key" />
           <button type="submit">Regenerate</button>
         </Form>
       </details>
-      <ul>
-        {data.project.labels.map((label) => (
-          <li key={label.id}>
-            <Link to={label.id}>{label.key}</Link>
-          </li>
-        ))}
-      </ul>
+      <h2>Labels</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>key</th>
+            <th>description</th>
+            {data.project.locales.map((locale) => (
+              <th key={locale.id}>{locale.name}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.project.labels.map((label) => (
+            <tr key={label.id}>
+              <td>
+                <Link to={label.id}>{label.key}</Link>
+              </td>
+              <td>{label.description ?? "-"}</td>
+              {data.project.locales.map((locale) => {
+                const translation = label.translations.find(
+                  (t) => t.localeId === locale.id
+                );
+
+                return <td key={label.id + locale.id}>{translation?.value}</td>;
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <Link to="create-label">Create label</Link>
     </div>
   );
