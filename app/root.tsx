8239ce2edc88a7999e380ca962079a7d3e140f21
Emilio Schepis <emilio.schepis@gmail.com>
2022-07-23 16:02:35 +0200

    feat: get locale from request

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,9 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type {
+  LinksFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -6,9 +11,15 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { useTranslation } from "react-i18next";
+import { useChangeLanguage } from "remix-i18next";
 
+import i18next from "./lib/i18n.server";
 import tailwind from "./styles/tailwind.css";
+
+type LoaderData = { locale: string };
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -20,9 +31,19 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwind }];
 };
 
+export let loader: LoaderFunction = async ({ request }) => {
+  const locale = await i18next.getLocale(request);
+  return json<LoaderData>({ locale });
+};
+
 export default function App() {
+  const data = useLoaderData<LoaderData>();
+  const { i18n } = useTranslation();
+
+  useChangeLanguage(data.locale);
+
   return (
-    <html lang="en" className="h-full bg-gray-100">
+    <html lang={data.locale} dir={i18n.dir()} className="h-full bg-gray-100">
       <head>
         <Meta />
         <Links />
