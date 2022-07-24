e7a51317de2f47616d987818ff80502fba77a071
Emilio Schepis <emilio.schepis@gmail.com>
2022-07-24 11:53:22 +0200

    feat: confirm deleting locales

--- app/routes/dashboard/$project/locales/index.tsx
+++ app/routes/dashboard/$project/locales/index.tsx
@@ -1,6 +1,9 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { ExclamationIcon } from "@heroicons/react/outline";
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, Link, useLoaderData } from "@remix-run/react";
+import { Form, Link, useLoaderData, useTransition } from "@remix-run/react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { db } from "~/lib/db.server";
@@ -73,6 +76,15 @@ export const loader: LoaderFunction = async ({ params }) => {
 export default function LocalesRoute() {
   const { t } = useTranslation();
   const data = useLoaderData<LoaderData>();
+  const transition = useTransition();
+  const [deleting, setDeleting] = useState<string | null>(null);
+  const cancelButtonRef = useRef(null);
+
+  useEffect(() => {
+    if (transition.state === "idle") {
+      setDeleting(null);
+    }
+  }, [transition.state]);
 
   return (
     <div className="py-4">
@@ -148,22 +160,14 @@ export default function LocalesRoute() {
                         </td>
                         <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                           <div className="flex items-center justify-end gap-2">
-                            <Form method="post">
-                              <input
-                                type="hidden"
-                                name="intent"
-                                value="delete-locale"
-                              />
-                              <input
-                                type="hidden"
-                                name="localeId"
-                                value={locale.id}
-                              />
-                              <button type="submit" className="text-red-600">
-                                {t("generic.delete")}
-                                <span className="sr-only">, {locale.name}</span>
-                              </button>
-                            </Form>
+                            <button
+                              type="button"
+                              className="text-red-600"
+                              onClick={() => setDeleting(locale.id)}
+                            >
+                              {t("generic.delete")}
+                              <span className="sr-only">, {locale.name}</span>
+                            </button>
                           </div>
                         </td>
                       </tr>
@@ -175,6 +179,88 @@ export default function LocalesRoute() {
           </div>
         </div>
       </div>
+      <Transition.Root show={!!deleting} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          initialFocus={cancelButtonRef}
+          onClose={() => setDeleting(null)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationIcon
+                        className="h-6 w-6 text-red-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg font-medium leading-6 text-gray-900"
+                      >
+                        {t("page.project.locales.delete.title")}
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          {t("page.project.locales.delete.description")}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                    <Form method="post">
+                      <input
+                        type="hidden"
+                        name="intent"
+                        value="delete-locale"
+                      />
+                      <input type="hidden" name="localeId" value={deleting!} />
+                      <button
+                        type="submit"
+                        className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                      >
+                        {t("generic.delete")}
+                      </button>
+                    </Form>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
+                      onClick={() => setDeleting(null)}
+                      ref={cancelButtonRef}
+                    >
+                      {t("generic.cancel")}
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </div>
   );
 }
